77ed37faea38eac9da1bda99ca59e998dde023bd
Matthew Matz <matt.m.matz@gmail.com>
2019-08-08 21:28:08 -0700

    Clear localStorage after project is loaded

--- editor.js
+++ editor.js
@@ -371,8 +371,9 @@ $(document).ready(function () {
 
         } else if (window.localStorage.getItem('localProject')) {
             // Look for a default project in the local browser store
-            setupWorkspace(JSON.parse(window.localStorage.getItem('localProject')));
-            //window.localStorage.removeItem('localProject');
+            setupWorkspace(JSON.parse(window.localStorage.getItem('localProject')), function () {
+                window.localStorage.removeItem('localProject');
+            });
         } else {
             // TODO: why is this here?
             // Open save-as modal
@@ -564,7 +565,7 @@ function resetToolBoxSizing(resizeDelay) {
  *
  * @param data
  */
-var setupWorkspace = function (data) {
+var setupWorkspace = function (data, callback) {
     console.log(data);
     projectData = data;
 
@@ -620,6 +621,7 @@ var setupWorkspace = function (data) {
     resetToolBoxSizing();
     timestampSaveTime(20, true);
     setInterval(checkLastSavedTime, 60000);
+    if (callback) callback();
 }
 
 
@@ -914,7 +916,8 @@ var editProjectDetails = function () {
             projectData.code = getXml();
         }
 
-        window.localStorage.setItem('localProject', JSON.stringify(projectData));
+        // TODO: I think these can be deleted.  localProject should only be used to temporarily store a project when the window location is changed or the page is refreshed.
+        //window.localStorage.setItem('localProject', JSON.stringify(projectData));
         //window.location = 'projectcreate.html?edit=true';
 
         $('#new-project-board-dropdown').addClass('hidden');
